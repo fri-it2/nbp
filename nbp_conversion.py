62dd8bbc1db4ce37d9136f220621744f5a3763fc
S56ZSA <s56zsa@hamradio.si>
2019-07-14 16:20:24 +0200

    add comments

--- nbp_conversion.py
+++ nbp_conversion.py
@@ -1,20 +1,33 @@
 import string
 chs = string.digits + string.ascii_uppercase
 chs_len=len(chs)
-def (start, chs):
+def t(start, chs):
+    """Convert decimal address to ascci address
+
+    :param start: NBP adress in decimal:Big Endian Byte Order: The most significant byte (the "big end") of the data is placed at the byte with the lowest address
+    :param chs:tsring of all digits and ascii upper
+    :return:NBP adress in ASCII
+    """
+
     if start == 0:
         return ''
     chs_len = len(chs)
     ch = chs[start%chs_len]
     return ch + t(start//chs_len, chs)
 def pretvorba(naslov, chs,number):
+    """convert ascci NBP adress to decimal NBP adress
 
+    :param naslov: NBP address ASCII: Big Endian Byte Order: The most significant byte (the "big end") of the data is placed at the byte with the lowest address
+    :param chs: string of all digits and ascii upper
+    :param number:
+    :return: NBP in decimal
+    """
     number = (number+chs.index(naslov[0]))*chs_len
     naslov = naslov[1:]
     if naslov == "":
         return number / chs_len
 #    import pdb
-#    pdb.set_trace()
+ #   pdb.set_trace()
     return pretvorba(naslov,chs, number)
 
 r=t(653331952, chs)
