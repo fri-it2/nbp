7070466dcc183a0fb7889aba5f09e215ed166738
S56ZSA <s56zsa@hamradio.si>
2019-09-02 16:53:30 +0200

    Update nbp_conversion.py
    conversion between number system

--- nbp_conversion.py
+++ nbp_conversion.py
@@ -1,10 +1,12 @@
 import string
+import pdb
+import numpy as np
 chs = string.digits + string.ascii_uppercase
 chs_len=len(chs)
-def t(start, chs):
+def convert36ToAscii(start, chs):
     """Convert decimal address to ascci address
 
-    :param start: NBP adress in decimal:Big Endian Byte Order: The most significant byte (the "big end") of the data is placed at the byte with the lowest address
+    :param start: NBP adress in decimal reverse:Big Endian Byte Order: The most significant byte (the "big end") of the data is placed at the byte with the lowest address
     :param chs:tsring of all digits and ascii upper
     :return:NBP adress in ASCII
     """
@@ -13,11 +15,11 @@ def t(start, chs):
         return ''
     chs_len = len(chs)
     ch = chs[start%chs_len]
-    return ch + t(start//chs_len, chs)
-def pretvorba(naslov, chs,number):
-    """convert ascci NBP adress to decimal NBP adress
+    return ch + convert36ToAscii(start//chs_len, chs)
+def convertAsciiTo36(naslov, chs,number):
+    """convert ascci NBP adress to decimal NBP address
 
-    :param naslov: NBP address ASCII: Big Endian Byte Order: The most significant byte (the "big end") of the data is placed at the byte with the lowest address
+    :param naslov: NBP address ASCII reverse: Big Endian Byte Order: The most significant byte (the "big end") of the data is placed at the byte with the lowest address
     :param chs: string of all digits and ascii upper
     :param number:
     :return: NBP in decimal
@@ -28,18 +30,82 @@ def pretvorba(naslov, chs,number):
         return number / chs_len
 #    import pdb
  #   pdb.set_trace()
-    return pretvorba(naslov,chs, number)
+    return convertAsciiTo36(naslov,chs, number)
+def convertDecBin(num):
+    """
 
-r=t(653331952, chs)
-print(r)
-r=t(64716160330,chs)
-naslov="S50PTP"
-naslov=naslov[::-1]
-s=pretvorba("ASZ65S",chs,0)
-s1=pretvorba("S56ZSA",chs,0)
-s2=pretvorba(naslov,chs,0)
-print(r)
+    :param num  NBP adress in dec:
+    :return: NBP in binary
+    """
+
+    return
+def convertBinDec(num):
+    """convert binary NBP adress to decimal NBP address
+
+    :param num:
+    :return:
+    """
+
+    return
+
+def convertOctDec(num):
+    """ convert octal NBP address to dec NBP address
+
+    :param num: 4x octal NBP adrdress [13,13,14,12], 32 bit, 4 x8 bit
+    :return : decimal NBP address
+    """
+    if len(num)!=4:
+        print num
+        return 0
+    return np.sum(np.array([1,256,256*256,256*256*256])*np.array(num))
+
+
+
+
+def convertNumber32Bit(stevilo, delitelj):
+    """ convert dec NBP to 32 bit number: 4 x 8bit in dec
+
+    :param stevilo: decimal NBP
+    :param delitelj: 256*256*256
+    :param naslov: NBP in 32 bit: 4x 8bit, array
+    :return:
+
+    Example
+    convertNumber32bit(653331952,256*256*256)
+    """
+    deljenec=stevilo / delitelj
+    ostanek = stevilo % delitelj
+    try:
+        naslov
+    except NameError:
+        naslov = []
+
+    naslov.append(deljenec)
+    #print
+    #pdb.set_trace()
+    if ostanek <= 256:
+        naslov.append(ostanek)
+
+        return naslov
+    #print(naslov)
+    #pdb.settrace()
+
+    return naslov+convertNumber32Bit(ostanek,delitelj/256)
+
+#r=convertDecAscii(653331952, chs)
+#print(r)
+#r=convertDecAscii(64716160330,chs)
+#naslov="S50PTP"
+#naslov=naslov[::-1]
+s1=convertAsciiTo36("ASZ65S",chs,0)
+#s1=convertAsciiTo36("S56ZSA",chs,0)
+#s2=convertAsciiTo36(naslov,chs,0)
+#print(r)
 print(s1)
-print(s)
-print(s2)
-print(t(1561529872,chs))
+#print(s)
+#print(s2)
+#print(convertDecAscii(1561529872,chs))
+#a=convertNumber32Bit(653331952,256*256*256)
+#print(a[::-1])
+#a=convertOctDec([ 240,13,241,38])
+#print(a)
